e589ff4e7c94bc9f1979cd3d5b65d87eed03ee6b
floyd <facundomartinezesc@gmail.com>
2021-01-22 13:50:56 -0300

    buscaminas finished

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { CellState, CellValue } from '../../types'
+import './Button.scss'
+
+interface ButtonProps {
+ row: number;
+ col:  number;
+ red?: boolean 
+ state: CellState;
+ value: CellValue;
+ onClick(rowParam: number, colParam: number): (...args: any[]) => void;
+ onContext(rowParam: number, colParam: number): (...args: any[]) => void;
+}
+
+const Button: React.FC<ButtonProps> = ({row, col, red, onContext,onClick, state, value}) => {
+
+  const renderContent = (): React.ReactNode => {
+    if(state === CellState.visible){
+      if(value === CellValue.bomb){
+        return (<span role='img' aria-label='bomb'>💣</span>)
+      } else if (value === CellValue.none) {
+        return null
+      }
+      return value;
+    } else if (state === CellState.flagged){
+      return (<span role='img' aria-label='bomb'>🚩</span>)
+    }
+    return null
+  }
+
+  return (
+    <div className={`Button ${state === CellState.visible ? 'visible' : "" } value-${value} ${red ? 'red' : ''}`} onClick={onClick(row, col)} onContextMenu={onContext(row, col)}>
+      {renderContent()}
+    </div>
+      
+  
+  )
+}
+
+export default Button
